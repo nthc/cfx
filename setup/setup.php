5ee8d58e72214071b3c6c20badb716c6b9e115e3
Ekow Abaka Ainooson <jainooson@gmail.com>
2012-05-20 23:57:34 +0000

    Worked on the setup scripts

--- setup/setup.php
+++ setup/setup.php
@@ -1,5 +1,9 @@
 #!/usr/bin/env php
 <?php
+error_reporting(E_ALL ^ E_NOTICE ^ E_WARNING);
+
+require "lib/Db.php";
+
 echo <<< WELCOME
 
 Welcome to the WYF Framework
@@ -9,16 +13,46 @@ as fast as possible.
 
 WELCOME;
 
-$name =     get_response("What is the name of your application", null, null, true);
-$host =     get_response("Where is your application's database hosted", 'localhost', null, true);
-$port =     get_response("What is the port of this database", '5432', null, true);
-$username = get_response("What is the database username", null, null, true);
-$password = get_response("What is the password for the database");
-$database = get_response("What is the name of your application's database (please ensure that the database exists)", null, null, true);
-$home =     get_response("Where is your application residing", getcwd(), null, null, true) . "/";
-$prefix =   get_response("What is the prefix of your application (Enter 'no prefix' if you do not want a prefix)", basename($home));
+$name = get_response(
+    "What is the name of your application", 
+    null, null, true
+);
 
-echo "\nSetting up the application ...\n";
+$home = get_response("Where is your application residing", getcwd(), null, null, true) . "/";
+$prefix = get_response("What is the prefix of your application (Enter 'no prefix' if you do not want a prefix)", basename($home));
+$db = get_db_credentials();
+
+do
+{
+    try{
+        echo "\nTesting your database connection ... ";
+        @Db::get($db);
+        $failed = false;
+    }
+    catch(Exception $e)
+    {
+        //fputs(STDERR, "Failed\n Could not establish a connection to the database.");
+        $response = get_response(
+            "Failed\nCould not establish a connection to the database. Would you like to provide new credentials", 
+            'yes', array('yes','no'), true
+        );
+        
+        if($response == 'yes')
+        {
+            echo "Getting new credentials ...\n";
+            $db = get_db_credentials();        
+            $failed = true;
+        }
+        else
+        {
+            exit();
+        }
+    }
+} while($failed);
+
+echo("OK");
+
+echo "\nSetting up the configuration files ...\n";
 
 mkdir2($home . 'app');
 mkdir2($home . 'app/cache');
@@ -32,9 +66,9 @@ mkdir2($home . 'app/temp');
 mkdir2($home . 'app/themes');
 mkdir2($home . 'app/uploads');
 
-copyDir("lib/setup/factory/*", "$home/app");
+copy_dir("lib/setup/factory/*", "$home/app");
 copy("lib/setup/htaccess", ".htaccess");
-file_put_contents(
+create_file(
     "$home/app/cache/menus/side_menu_1.html",
     str_replace(
         '{$prefix}', 
@@ -50,7 +84,7 @@ $system = <<< SYSTEM
 \$package_path = "system";
 \$package_schema = "system";
 SYSTEM;
-file_put_contents($home . 'app/modules/system/package_redirect.php', $system);
+create_file($home . 'app/modules/system/package_redirect.php', $system);
 
 $index = <<< "INDEX"
 <?php
@@ -60,7 +94,8 @@ $index = <<< "INDEX"
  */
 require "lib/entry.php";
 INDEX;
-file_put_contents($home . 'index.php', $index);
+
+create_file($home . 'index.php', $index);
 
 $config = <<< "CONFIG"
 <?php
@@ -73,11 +108,11 @@ $config = <<< "CONFIG"
     'db' => array(
     	"main" => array(
             'driver' => 'postgresql',
-            'user' => '$username',
-            'host' => '$host',
-            'password' => '$password',
-            'name' => '$database',
-            'port' => '$port'    	
+            'user' => '{$db['user']}',
+            'host' => '{$db['host']}',
+            'password' => '{$db['password']}',
+            'name' => '{$db['name']}',
+            'port' => '{$db['port']}'    	
     	)
     ),
     'cache' => array(
@@ -88,10 +123,12 @@ $config = <<< "CONFIG"
     'theme' => 'default'
 );
 CONFIG;
-file_put_contents($home . 'app/config.php', $config);
-file_put_contents($home . 'app/includes.php', "<?php\n");
-file_put_contents($home . 'app/bootstrap.php', "<?php\n");
 
+create_file($home . 'app/config.php', $config);
+create_file($home . 'app/includes.php', "<?php\n");
+create_file($home . 'app/bootstrap.php', "<?php\n");
+
+// Try to initialize the wyf framework.
 require "lib/wyf_bootstrap.php";
 
 echo "\nSetting up the database ...\n";
@@ -113,6 +150,27 @@ Db::query(
 );
 
 echo "\nDone! Happy programming ;)\n\n";
+
+/**
+ * A utility function for creating files. Checks if the files are writable and
+ * goes ahead to create them. If they are not it just dies!
+ */
+function create_file($file, $contents)
+{
+    if(is_writable(dirname($file)))
+    {
+        file_put_contents($file, $contents);
+        return true;
+    }
+    else
+    {
+        fputs(
+            STDERR,
+            "Error writing to file $file. Please ensure you have the correct permissions"
+        );
+        return false;
+    }
+}
 
 /**
  * A function for getting answers to questions from users interractively.
@@ -169,7 +227,7 @@ function mkdir2($path)
     if(!\is_writable(dirname($path)))
     {
         fputs(STDERR, "You do not have permissions to create the $path directory\n");
-        $path = false;
+        die();
     }
     else if(\is_dir($path))
     {
@@ -182,7 +240,7 @@ function mkdir2($path)
     return $path;
 }
 
-function copyDir($source, $destination)
+function copy_dir($source, $destination)
 {
     foreach(glob($source) as $file)
     {
@@ -191,13 +249,25 @@ function copyDir($source, $destination)
         if(is_dir($file))
         {
             mkdir2($newFile);
-            copyDir("$file/*", $newFile);
+            copy_dir("$file/*", $newFile);
         }
         else
         {
             copy($file, $newFile);
         }
     }
+}
+
+function get_db_credentials()
+{
+    $db = array();
+    $db['host'] =     get_response("Where is your application's database hosted", 'localhost', null, true);
+    $db['port'] =     get_response("What is the port of this database", '5432', null, true);
+    $db['user'] = get_response("What is the database username", null, null, true);
+    $db['password'] = get_response("What is the password for the database");
+    $db['name'] = get_response("What is the name of your application's database (please ensure that the database exists)", null, null, true);
+    
+    return $db;
 }
 
 
