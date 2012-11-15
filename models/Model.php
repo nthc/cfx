2f65b2a195479325f8203763ce16676e0aa5ed83
Ekow Abaka Ainooson <jainooson@gmail.com>
2012-11-15 12:28:41 +0000

    Experimenting with the new conditions code

--- models/Model.php
+++ models/Model.php
@@ -777,4 +777,48 @@ abstract class Model implements ArrayAccess
         $this->queryExplicitRelations = $queryExplicitRelations;
         return $this;
     }
+    
+    /**
+     * 
+     * 
+     * @param type $conditionArray
+     * @return string
+     */
+    public static function condition($conditionArray)
+    {
+        foreach($conditionArray as $field => $condition)
+        {
+            if(is_array($condition))
+            {
+                foreach($condition as $clause)
+                {
+                    $conditions[] = "$field = '$clause'";
+                }
+            }
+            else
+            {
+                preg_match("/(?<field>[a-zA-Z1-9_.]*)\w*(?<operator>\>=|\<=|\<\>|\<|\>)?/", $field, $matches);
+                $databaseField = $matches['field'];//$this->resolveName($matches["field"]);
+
+                if($condition === null)
+                {
+                    $operator = 'is';
+                }
+                else
+                {
+                    $operator = $matches["operator"]==""?"=":$matches["operator"];
+                }
+
+                $condition = $condition === null ? 'NULL' : "'" . Db::escape($condition) . "'";
+                $conditions[] = "$databaseField $operator $condition";
+            }
+        }
+        
+        if(is_array($conditions))
+        {
+            $compiled = implode(" AND ", $conditions);
+        }
+        
+        return $compiled;
+    }
 }
