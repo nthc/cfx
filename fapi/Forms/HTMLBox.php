8edc043cc959046901e83887619ca212824c236f
Ekow Abaka Ainooson <jainooson@gmail.com>
2014-02-20 20:42:44 +0000

    Support added for the HTML box

--- fapi/Forms/HTMLBox.php
+++ fapi/Forms/HTMLBox.php
@@ -21,6 +21,12 @@ class HTMLBox extends Container
         $this->content  = $content;    
     }
     
+    public function setContent($content)
+    {
+        $this->content = $content;
+        return $this;
+    }
+    
     public function render()
     {
         return $this->content;
