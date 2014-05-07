ab445ea789ff629b54622f9443ef1a2782736d9d
Ekow Abaka Ainooson <jainooson@gmail.com>
2014-05-07 09:47:53 +0000

    Worked on the custom session handler

---
+++ Sessions.php
@@ -0,0 +1,112 @@
+<?php
+class Sessions
+{
+    private $new = false;
+    private $lifespan = 600;
+    private $id;
+    private static $handler = false;
+    
+    public static function getHandler()
+    {
+        if(self::$handler === false)
+        {
+            self::$handler = new Sessions();
+        }
+        
+        return self::$handler;
+    }
+    
+    public function open($sessionPath, $sessionName)
+    {
+        
+    }
+    
+    public function write($sessionId, $data)
+    {
+        if($this->new)
+        {
+            Db::query(
+                sprintf(
+                    "INSERT into system.sessions(id, data, expires, lifespan) VALUES('%s', '%s', %d, %d)",
+                    $sessionId, 
+                    Db::escape($data), 
+                    time() + $this->lifespan, 
+                    $this->lifespan
+                )
+            );
+        }
+        else
+        {
+            Db::query(
+                sprintf(
+                    "UPDATE system.sessions SET data = '%s', expires = %d WHERE id = '%s'",
+                    db::escape($data), time() + $this->lifespan, $sessionId
+                )
+            );
+        }
+        return true;
+    }
+    
+    public function getId()
+    {
+        return $this->id;
+    }
+    
+    public static function bindUser($userId)
+    {
+        if(Application::$config['custom_sessions'])
+        {
+            Db::query(sprintf("DELETE FROM system.sessions WHERE user_id = %d", $userId));
+            Db::query(sprintf("UPDATE system.sessions SET user_id = %d WHERE id = '%s'", $userId, self::getHandler()->getId()));
+        }
+    }
+    
+    public function read($sessionId)
+    {
+        $this->id = $sessionId;
+        $result = reset(
+            Db::query(
+                sprintf("SELECT data, lifespan, expires FROM system.sessions WHERE id = '%s'", $sessionId, time())
+            )
+        );
+        if($result['expires'] <= time())
+        {
+            Db::query(sprintf("DELETE FROM system.sessions WHERE id = '%s'", $sessionId));
+            $this->new = true;
+            return '';
+        }
+        else if(count($result) == 0)
+        {
+            $this->new = true;
+            return '';
+        }
+        else
+        {
+            $this->lifeSpan = $result['lifespan'];
+            return $result['data'];
+        }
+    }
+    
+    public function close()
+    {
+        return true;
+    }
+    
+    public function destroy($sessionId)
+    {
+        Db::query(sprintf("DELETE FROM system.sessions WHERE id = '%s'", $sessionId));
+        return true;        
+    }
+    
+    public function gc($lifetime)
+    {
+        Db::query(sprintf("DELETE FROM system.sessions WHERE expiry < %d", time()));
+        return true;
+    }
+    
+    public function isNew()
+    {
+        return $this->new;
+    }
+}
+
--- entry.php
+++ entry.php
@@ -42,9 +42,25 @@ if(isset($_REQUEST["__api_session_id"]))
 /**
  * Initialize the session handler
  */
-session_start();
 
 require "wyf_bootstrap.php";
+
+if(Application::$config['custom_sessions'])
+{
+    $handler = Sessions::getHandler();
+    session_set_save_handler
+    (
+        array($handler, 'open'), 
+        array($handler, 'close'), 
+        array($handler, 'read'), 
+        array($handler, 'write'), 
+        array($handler, 'destroy'), 
+        array($handler, 'gc')
+    );
+    register_shutdown_function('session_write_close');
+}
+
+session_start();
 
 $authExcludedPaths = array(
     "system/login",
