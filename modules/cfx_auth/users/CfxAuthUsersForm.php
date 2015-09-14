28a4d0a2d08cbfc9245b12133aad401f83da9a9d
Dela Kanda <dkanda@nthc.com.gh>
2015-09-14 12:47:07 +0000

    added form and validate rules to users

--- modules/cfx_auth/users/CfxAuthUsersForm.php
+++ modules/cfx_auth/users/CfxAuthUsersForm.php
@@ -2,4 +2,18 @@
 
 class CfxAuthUsersForm extends Form{
     
+    public function __construct()
+    {
+        parent::__construct();
+        $this->add(
+            Element::create("TextField", "Username", "user_name"),
+            Element::create("TextField", "Firstname", "first_name"),
+            Element::create("TextField", "Lastname", "last_name"),
+            Element::create("TextField", "Othernames", "other_names"),
+            Element::create("TextField", "Email", "email"),
+            Element::create("HiddenField", "password")
+        );
+        $this->addAttribute("style", "width:450px");
+    }
+    
 }
